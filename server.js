6099ef70024b1d652042858b7420896ca44f6267
BenjmnWong <112783407+BenjmnWong@users.noreply.github.com>
2025-01-24 14:04:39 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch'); // For server-side HTTP requests
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
